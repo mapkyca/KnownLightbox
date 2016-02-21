10ccca2ca718cf5dea25996fc94b7d96a48a02b5
Marcus Povey <marcus@marcus-povey.co.uk>
2016-02-21 13:10:32 +0000

    Tidying lightbox code

--- Lightbox/Main.php
+++ Lightbox/Main.php
@@ -5,11 +5,11 @@ namespace IdnoPlugins\LightBox {
     class Main extends \Idno\Common\Plugin {
 
 	function registerPages() {
-	    // Add custom CSS and js
-	    \Idno\Core\site()->template()->extendTemplate('shell/head', 'lightbox/shell/head');
-
-	    // Tracking code
-	    \Idno\Core\site()->template()->extendTemplate('shell/footer', 'lightbox/shell/footer');
+	    
+	    // Lightbox CSS and init code
+	    \Idno\Core\Idno::site()->template()->extendTemplate('shell/head', 'lightbox/shell/head');
+	    \Idno\Core\Idno::site()->template()->extendTemplate('shell/footer', 'lightbox/shell/footer');
+	    
 	}
 
     }
