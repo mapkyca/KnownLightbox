a7534a145fc85cffed5dc958c0e9399f71c9c8db
Marcus Povey <marcus@marcus-povey.co.uk>
2016-02-21 13:11:51 +0000

    Correcting links, removing comments

--- Lightbox/templates/default/lightbox/shell/footer.tpl.php
+++ Lightbox/templates/default/lightbox/shell/footer.tpl.php
@@ -1,4 +1,4 @@
-<script src="<?= \Idno\Core\site()->config()->url; ?>IdnoPlugins/LightBox/vendor/ekko-lightbox.min.js"></script>
+<script src="<?= \Idno\Core\site()->config()->url; ?>IdnoPlugins/Lightbox/vendor/ekko-lightbox.min.js"></script>
 
 <script type="text/javascript">
     $("div.idno-entry-photo img.u-photo").parent('a').click(function() {
--- Lightbox/templates/default/lightbox/shell/head.tpl.php
+++ Lightbox/templates/default/lightbox/shell/head.tpl.php
@@ -1,3 +1,1 @@
-<link rel="stylesheet" href="<?=\Idno\Core\site()->config()->url;?>IdnoPlugins/LightBox/vendor/ekko-lightbox.min.css" type="text/css" media="screen" />
-<?php /* <link rel="stylesheet" href="<?=\Idno\Core\site()->config()->url;?>IdnoPlugins/LightBox/vendor/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" /> */ ?>
-
+<link rel="stylesheet" href="<?=\Idno\Core\site()->config()->url;?>IdnoPlugins/Lightbox/vendor/ekko-lightbox.min.css" type="text/css" media="screen" />
