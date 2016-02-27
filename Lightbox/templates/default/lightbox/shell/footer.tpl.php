69f148f9530b195ebdcdb668a297628a73318b86
Marcus Povey <marcus@marcus-povey.co.uk>
2016-02-27 15:22:01 +0000

    Fixing lightbox activation

--- Lightbox/templates/default/lightbox/shell/footer.tpl.php
+++ Lightbox/templates/default/lightbox/shell/footer.tpl.php
@@ -1,7 +1,7 @@
 <script src="<?= \Idno\Core\site()->config()->url; ?>IdnoPlugins/Lightbox/vendor/ekko-lightbox.min.js"></script>
 
 <script type="text/javascript">
-    $("div.idno-entry-photo img.u-photo").parent('a').click(function() {
+    $("div.idno-entry-photo .entry-content img.u-photo").parent('a').click(function() {
 	event.preventDefault();
 	$(this).ekkoLightbox();
     });
