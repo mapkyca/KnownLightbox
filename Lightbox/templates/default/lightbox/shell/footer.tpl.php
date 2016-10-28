40d7f1e644077eedc48588499d995d8fd2bf22d8
Marcus Povey <marcus@marcus-povey.co.uk>
2016-10-28 12:04:30 +0100

    Fixing Lightbox to handle new photo link format

--- Lightbox/templates/default/lightbox/shell/footer.tpl.php
+++ Lightbox/templates/default/lightbox/shell/footer.tpl.php
@@ -3,6 +3,7 @@
 <script type="text/javascript">
     $("div.idno-entry-photo .entry-content img.u-photo").parent('a').click(function() {
 	event.preventDefault();
+	$("div.idno-entry-photo .entry-content img.u-photo").parent('a').attr('href', $("div.idno-entry-photo .entry-content img.u-photo").attr('src'));
 	$(this).ekkoLightbox();
     });
 
